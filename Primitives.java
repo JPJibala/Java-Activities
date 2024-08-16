38ec6150f0b9bd5f34346ab09faf400a3cfc0c87
John Peter Jibala <127470595+JPJibala@users.noreply.github.com>
2024-08-16 11:21:43 +0000

    Task 1-1-3 - Done

---
+++ Primitives.java
@@ -0,0 +1,16 @@
+public class Primitives {
+    public static void main(String[] args) {
+        // Declare primitive variables
+        byte zero = 0;
+        short three = 3;
+        int one = 1;
+        float twoPointZero = 2.0f;
+        boolean isTrue = true;
+
+        // Concatenate primitives into a string to form "H3110 w0r1d 2.0 true"
+        String output = "H" + three + one + one + zero + " w" + zero + "r" + one + "d " + twoPointZero + " " + isTrue;
+
+        // Print the result
+        System.out.println(output);
+    }
+}
