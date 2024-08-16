102174a4b0bdccc39f40c1ed0c8a5feeb4f48ce9
John Peter Jibala <127470595+JPJibala@users.noreply.github.com>
2024-08-16 11:34:44 +0000

    Task 1-1-4 - Done

---
+++ ControlFlowUsingLoops.java
@@ -0,0 +1,13 @@
+public class ControlFlowUsingLoops {
+    public static void main(String[] args) {
+        //Create int varialble named check_number with value of 10.
+        int check_number = 10;
+        String message;
+
+        //Using For loop to check/print if number is odd or even
+        for (int i = 1; i <= check_number; i++) {
+            message = (i % 2 == 0) ? i + " is even number." : i + " is odd number.";
+            System.out.println(message);
+        }
+    }
+}
