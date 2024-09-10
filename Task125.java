ad61a4ff8c266c6d95f947710dbbf8d53ddd8196
John Peter Jibala <127470595+JPJibala@users.noreply.github.com>
2024-09-10 12:22:26 +0000

    Task 1-2-5 : Done

---
+++ Task125.java
@@ -0,0 +1,43 @@
+// Base class Vehicle
+class Vehicle {
+    protected String make;
+    protected String model;
+    protected int year;
+
+    // Constructor for Vehicle class
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+}
+
+// Derived class Car that extends Vehicle
+class Car extends Vehicle {
+    private int numberOfDoors;
+
+    // Constructor for Car class
+    public Car(String make, String model, int year, int numberOfDoors) {
+        super(make, model, year);  // Call the constructor of the Vehicle class
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    // Method to display details of the Car
+    public void displayDetails() {
+        System.out.println("Car Details:");
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Number of Doors: " + numberOfDoors);
+    }
+}
+
+public class Task125 {
+    public static void main(String[] args) {
+        // Create an instance of the Car class
+        Car car = new Car("Toyota", "Camry", 2022, 4);
+        
+        // Display the details of the car
+        car.displayDetails();
+    }
+}
