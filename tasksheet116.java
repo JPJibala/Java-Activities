6d4d86c299032e377abc5859f47c363a875040d4
John Peter Jibala <127470595+JPJibala@users.noreply.github.com>
2024-09-21 03:03:04 +0000

    tasksheet-1-1-6 : Done

--- tasksheet116.java
+++ tasksheet116.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PalindromeChecker {
+public class tasksheet116 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter a string : ");
