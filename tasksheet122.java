e3240a787cc07fc5d20646caad8ff3c3eafddb52
John Peter Jibala <127470595+JPJibala@users.noreply.github.com>
2024-09-21 03:12:37 +0000

    tasksheet-1-2-2 : Done

--- tasksheet122.java
+++ tasksheet122.java
@@ -13,7 +13,7 @@ class Student {
     }
 }
 
-public class Tasksheet122 {
+public class tasksheet122 {
     public static void main(String[] args) {
         Student[] students = new Student[] {
             new Student("Morgan", "Freeman"),
